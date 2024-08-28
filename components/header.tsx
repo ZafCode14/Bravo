5e12f1d0d431002dba079e3a940d77d6368d11ca
ZafCode14 <zafarwp14@gmail.com>
2024-08-28 16:56:51 +0300

    header logo to full width and process section edit

--- components/header.tsx
+++ components/header.tsx
@@ -19,9 +19,15 @@ function Header() {
   };
   return (
     <header className={`w-full ${width > 1000 && "absolute top-0 z-10"} bg-color-black flex flex-col items-center justify-center`}>
-      <Image alt="logo" src={'/icons/bravowhite.svg'} height={"2000"} width={"1000"} className="w-[95%] max-w-[447px] h-auto mt-8"/>
-      <div className="w-[1200px] max-w-[95%] flex justify-end mt-[]">
-        <button onClick={handleLanguageChange} className="bg-[#CADB4A] w-[82px] h-[26px] text-white text-[12px] rounded-sm mt-5 mb-3 md:w-[100px] md:h-[31px] md:text-[15px]">{lang}</button>
+      <Image 
+        alt="logo" 
+        src={width < 1024 ? '/icons/bravowhite.svg' : '/icons/logoWhiteLong.svg'} 
+        height={"2000"} 
+        width={"1000"} 
+        className="w-[95%] max-w-[447px] lg:w-full lg:max-w-full h-auto mt-8"
+      />
+      <div className="w-[1200px] max-w-[95%] flex justify-end">
+        <button onClick={handleLanguageChange} className="bg-[#CADB4A] w-[82px] h-[26px] text-white text-[12px] rounded-sm mt-2 mb-4 md:w-[100px] md:h-[31px] md:text-[15px]">{lang}</button>
       </div>
     </header>
   );
