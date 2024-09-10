f4eda9bfed0eb176496a7fb6203a4ed46f9fa54b
ZafCode14 <zafarwp14@gmail.com>
2024-09-11 02:45:45 +0300

    nav process size

--- components/sectionsNav.tsx
+++ components/sectionsNav.tsx
@@ -5,7 +5,7 @@ interface secNavProp {
 function SectionsNav({ title, opacity }: secNavProp) {
   return (
     <div className={`flex w-full flex-col items-center md:mx-1 xl:mx-2 ${opacity}`}>
-      <p className="text-[10px] xl:text-[10px] font-bold mb-2">{title}</p>
+      <p className="text-[10px] xl:text-[9px] font-bold mb-2">{title}</p>
       <div className="w-[190px] h-[5px] bg-[#afafaf]">
         <div className={"bg-[#575757] h-full w-full"}>
         </div>
